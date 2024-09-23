52085054370a28ce162fd1d3b91165e952ad280d
ninanikolaevna <ninanikolaevna.t@gmail.com>
2024-09-23 23:31:51 +0300

    start

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main{
+    //ЗДЕСЬ ДЕЛАЮ ДРУГИЕ МЕТОДЫ
+   //            private static void checkLeap (year) {
+
+
+    //ЗДЕСЬ НАЧИНАЕТСЯ метод MAIN
+    public static void main(String[] args) {
+        //task1
+/*Реализуйте метод, который получает в качестве параметра год,
+проверяет, является ли он високосным, и выводит результат в консоль.
+ */
+        int year = 2020;
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + "год является високосным.");
+        } else {
+            System.out.println(year + "год не является високосным.");
+        }
+
+
+
+
+
+
+//task3
+        /*
+        int deliveryDistance = 95;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: 1");
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println("Потребуется дней:  2");
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется дней: 3");
+        } else {
+            System.out.println("Доставки нет");
+        }
+*/
+    } //тут конец метода main
+}
